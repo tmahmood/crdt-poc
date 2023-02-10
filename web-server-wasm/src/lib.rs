af537ed33b01367e96aae6b9d4ae346cb9dc3f39
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2023-02-10 15:45:21 +0600

    Added Changes from service worker branch in web-wasm module

--- web-server-wasm/src/lib.rs
+++ web-server-wasm/src/lib.rs
@@ -1,95 +1,73 @@
-mod utils;
-
-use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+use axum::{response::{Html, Response}, routing::get, Router, Error, http};
+use axum::body::{Body, Bytes};
+use axum::body::HttpBody;
+use http::{Request};
+use std::str;
+use tower_service::Service;
+// use js_sys::{Object, Reflect, WebAssembly};
+// use wasm_bindgen_futures::{spawn_local, JsFuture};
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+// const WA_SQLITE: &[u8] = include_bytes!("../wa-sqlite-async.wasm");
+//
+//
+// pub async fn load_module() -> Object {
+//     let a = JsFuture::from(WebAssembly::instantiate_buffer(WA_SQLITE, &Object::new())).await.unwrap();
+//     let b: WebAssembly::Instance = Reflect::get(&a, &"instance".into()).unwrap().dyn_into().unwrap();
+//     b.exports()
+// }
 
-#[wasm_bindgen]
-pub fn to_sql(js_value: JsValue) -> Result<JsValue, JsValue> {
-    let db: JsonToSql = serde_wasm_bindgen::from_value(js_value)?;
-    let mut v = vec![];
-    for table in db.tables {
-        v.push(table.to_sql());
-    }
-    Ok(serde_wasm_bindgen::to_value(&v)?)
-}
-
-#[wasm_bindgen]
-#[derive(Serialize, Deserialize)]
-pub struct JsonToSql {
-    db: String,
-    tables: Vec<TableDsl>
-}
 
 #[wasm_bindgen]
-impl JsonToSql {
-    pub fn db(&self) -> String {
-        self.db.clone()
-    }
-
-    pub fn tables(&self) -> *const TableDsl {
-        self.tables.as_ptr()
-    }
-}
-
-#[wasm_bindgen]
-#[derive(Serialize, Deserialize)]
-pub struct TableDsl {
-    name: String,
-    columns: Vec<ColumnDef>,
-}
-
-#[wasm_bindgen]
-impl TableDsl {
-    pub fn name(&self) -> String {
-        self.name.clone()
-    }
-    pub fn columns(&self) -> *const ColumnDef {
-        self.columns.as_ptr()
-    }
-}
-
-impl TableDsl {
-    pub fn to_sql(&self) -> (String, String) {
-        let mut q = vec![];
-        for column in self.columns.iter() {
-            let sql = column.to_sql();
-            q.push(sql);
+pub async fn make_request(uri: String) -> String {
+    let routes = vec![
+        "/",
+        "/earth",
+        "/mars",
+    ];
+    let mut found = 0;
+    for i in 0..routes.len() {
+        if uri == routes[i] {
+            found = 1;
+            break;
         }
-        (self.name.clone(), format!("CREATE TABLE IF NOT EXISTS {} (id primary key, {})", self.name, q.join(", ")))
     }
+    let mut router: Router = Router::new()
+        .route("/", get(index))
+        .route("/earth", get(earth))
+        .route("/mars", get(mars));;
+    if found == 0 {
+        return "".to_string();
+    }
+
+    let request: Request<Body> = Request::builder()
+        .uri(uri)
+        .body("".into())
+        .unwrap();
+
+    let mut response = router.call(request).await.unwrap();
+    let data: Option<Result<Bytes, Error>> = HttpBody::data(response.body_mut()).await;
+    let result: Bytes = data.unwrap().unwrap();
+    str::from_utf8(&*result).unwrap().to_string()
+
 }
 
-#[wasm_bindgen]
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct ColumnDef {
-    name: String,
-    column_type: String,
-    defs: Option<String>,
+async fn index() -> Html<&'static str> {
+    Html("<h1>Hello World!!!</h1><br/><a href=\"mars\">Hello Anyplace</a>")
 }
 
-#[wasm_bindgen]
-impl ColumnDef {
-    pub fn name(&self) -> String {
-        self.name.clone()
-    }
-    pub fn column_type(&self) -> String {
-        self.column_type.clone()
-    }
-    pub fn defs(&self) -> Option<String> {
-        self.defs.clone()
-    }
+async fn earth() -> Html<&'static str> {
+    Html("<h1>Hello World from Axum!!!</h1><br/><a href=\"mars\">Hello Mars</a>")
 }
 
-impl ColumnDef {
-    pub fn to_sql(&self) -> String {
-        format!("{} {} {}", self.name, self.column_type, self.defs.clone().unwrap_or("".to_string()))
-    }
+async fn mars() -> Html<&'static str> {
+    Html("<h1>Hello Mars from Axum!!!</h1></h1><br/><a href=\"/\">Hello Earth</a>")
 }
 
+
+// #[wasm_bindgen(start)]
+// pub fn run() {
+//     spawn_local(async {
+//         load_module().await;
+//     });
+// }
